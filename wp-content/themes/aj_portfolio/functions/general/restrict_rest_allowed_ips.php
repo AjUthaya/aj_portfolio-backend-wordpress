b27f2ee11b1e0b0afe3b9f17204025661fcded18
APL <ajanth160899@gmail.com>
2018-12-27 16:34:25 +0000

    Small changes

--- wp-content/themes/aj_portfolio/functions/general/restrict_rest_allowed_ips.php
+++ wp-content/themes/aj_portfolio/functions/general/restrict_rest_allowed_ips.php
@@ -11,12 +11,12 @@ function restrict_rest_allowed_ips($errors) {
   // 1. DEFINE: Allowed IP addresses
   $allowed_ips = array(
     // Local
-    '172.18.0.1'
+    '172.22.0.1'
   );
 
   // IF: Request IP is not in the allowed IP array
   if (!in_array($_SERVER['REMOTE_ADDR'], $allowed_ips)) {
-    return new WP_Error('forbidden_access', 'Request origin is not whitelisted', array(
+    return new WP_Error('forbidden_access', 'Request origin of ' . $_SERVER['REMOTE_ADDR'] . ' is not whitelisted', array(
       'status' => 403
     ));
   }
