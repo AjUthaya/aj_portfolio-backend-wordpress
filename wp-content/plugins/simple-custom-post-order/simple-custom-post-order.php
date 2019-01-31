4e891695e951caf93fd008ad666807f99b25950e
APL <ajanth160899@gmail.com>
2019-01-31 08:17:47 +0100

    Updated plugins

--- wp-content/plugins/simple-custom-post-order/simple-custom-post-order.php
+++ wp-content/plugins/simple-custom-post-order/simple-custom-post-order.php
@@ -1,12 +1,13 @@
 <?php
-
 /*
-  Plugin Name: Simple Custom Post Order
-  Plugin URI: https://wordpress.org/plugins-wp/simple-custom-post-order/
-  Description: Order Items (Posts, Pages, and Custom Post Types) using a Drag and Drop Sortable JavaScript.
-  Version: 2.4.0
-  Author: Colorlib
-  Author URI: https://colorlib.com/wp/
+ *Plugin Name: Simple Custom Post Order
+ *Plugin URI: https://wordpress.org/plugins-wp/simple-custom-post-order/
+ *Description: Order Items (Posts, Pages, and Custom Post Types) using a Drag and Drop Sortable JavaScript.
+ *Version: 2.4.1
+ *Author: Colorlib
+ *Author URI: https://colorlib.com/wp/
+ *Text Domain: simple-custom-post-order
+ *Domain Path: /languages/
  */
 
 
@@ -45,6 +46,11 @@ class SCPO_Engine {
         add_action( 'admin_notices', array( $this, 'scporder_notice_not_checked' ) );
         add_action( 'wp_ajax_scporder_dismiss_notices', array( $this, 'dismiss_notices' ) );
 
+        add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
+    }
+
+    public function load_plugin_textdomain(){
+        load_plugin_textdomain( 'simple-custom-post-order', false, basename( dirname( __FILE__ ) ) . '/languages/' );
     }
 
     public function dismiss_notices() {
@@ -82,12 +88,12 @@ class SCPO_Engine {
         <div class="notice scpo-notice" id="scpo-notice">
             <img src="<?php echo esc_url( plugins_url( 'assets/logo.jpg', __FILE__ ) ); ?>" width="80">
 
-            <h1><?php esc_html_e( 'Simple Custom Post Order', 'scporder' ); ?></h1>
+            <h1><?php esc_html_e( 'Simple Custom Post Order', 'simple-custom-post-order' ); ?></h1>
 
-            <p><?php esc_html_e( 'Thank you for installing our awesome plugin, in order to enable it you need to go to the settings page and select which custom post or taxonomy you want to order.', 'scporder' ); ?></p>
+            <p><?php esc_html_e( 'Thank you for installing our awesome plugin, in order to enable it you need to go to the settings page and select which custom post or taxonomy you want to order.', 'simple-custom-post-order' ); ?></p>
 
-            <p><a href="<?php echo admin_url( 'options-general.php?page=scporder-settings' ) ?>" class="button button-primary button-hero"><?php esc_html_e( 'Get started !', 'scporder' ); ?></a></p>
-            <button type="button" class="notice-dismiss"><span class="screen-reader-text"><?php esc_html_e( 'Dismiss this notice.', 'scporder' ); ?></span></button>
+            <p><a href="<?php echo admin_url( 'options-general.php?page=scporder-settings' ) ?>" class="button button-primary button-hero"><?php esc_html_e( 'Get started !', 'simple-custom-post-order' ); ?></a></p>
+            <button type="button" class="notice-dismiss"><span class="screen-reader-text"><?php esc_html_e( 'Dismiss this notice.', 'simple-custom-post-order' ); ?></span></button>
         </div>
 
         <style>
@@ -136,7 +142,7 @@ class SCPO_Engine {
     }
 
     public function admin_menu() {
-        add_options_page(__('SCPOrder', 'scporder'), __('SCPOrder', 'scporder'), 'manage_options', 'scporder-settings', array($this, 'admin_page'));
+        add_options_page(__('SCPOrder', 'simple-custom-post-order'), __('SCPOrder', 'simple-custom-post-order'), 'manage_options', 'scporder-settings', array($this, 'admin_page'));
     }
 
     public function admin_page() {
